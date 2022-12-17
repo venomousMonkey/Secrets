098905ea09fd5b792b624a8088c4da3ab4dd5bf7
Paweł Łuszkiewicz <matryca@gmail.com>
2022-12-17 14:58:47 +0100

    Using Passport and serving cookies in session

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const { setTheUsername } = require('whatwg-url');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
 //removing moongoose encryption as I'll switch to password hashing using md5
 //const encrypt = require('mongoose-encryption');   
 //swapping md5 for bcrypt
@@ -19,16 +23,34 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(session({
+    secret: "This is the secret.", 
+    resave: false, 
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+
 
 const userSchema = new mongoose.Schema ({
     email: String, 
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 //userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy()); 
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
     res.render('home');
@@ -40,6 +62,21 @@ app.get("/login", (req, res) => {
 
 app.get("/register", (req, res) => {
     res.render('register');
+});
+
+app.get("/secrets", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render('secrets'); 
+    } else {
+        res.redirect('/login');
+    }
+});
+
+app.get("/logout", (req,res) => {
+    req.logout((err) => {
+        if (err) { return next(err); };
+        res.redirect('/');
+})
 });
 
 app.post('/register', (req, res) => {
@@ -58,10 +95,21 @@ app.post('/register', (req, res) => {
 //         }
 //     });
 // });
+
+User.register({username: req.body.username}, req.body.password, (err, user) => {
+    if (err) {
+        console.log(err);
+        res.redirect('/register');
+    } else {
+        passport.authenticate('local')(req, res, () => {
+            res.redirect('/secrets');
+        })
+    }
+})
 });
 
 app.post('/login', (req, res) => {
-    
+
 //removing bcrypt in favor of passport
 //     const userName = req.body.username;
 //     const password = req.body.password;
@@ -80,6 +128,22 @@ app.post('/login', (req, res) => {
 //                 } 
 //         }
 //     })
+
+const user = new User({
+    username: req.body.username,
+    password: req.body.password
+});
+
+req.login(user, (err) => {
+    if (err) {
+        console.log(err); 
+    } else {
+        passport.authenticate('local')(req, res, () => {
+            res.redirect('/secrets');
+        })
+    }
+});
+
 
 });
 
